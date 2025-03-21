01b5cd82063b17f1eb40dd84bb7926d848e41eca
Si mohamed Nesrine <simohamed2004nesrine@gmail.com>
2025-03-21 17:30:50 +0100

    adding newpost

---
+++ SocialPulseCommunityTemp/WebApplication1/Controllers/dashboard/newPostController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SocialPulseCommunityWeb.Controllers.dashboard
+{
+    public class newPostController : Controller
+    {
+        public IActionResult newPost()
+        {
+            return View();
+        }
+    }
+}
